89766dcb415de0a48f132b7fcaf33997a07604d3
wyongjae <dnjsdydwo123@naver.com>
2023-04-10 13:35:24 +0900

    Add: AddEditNoteEvent and add method changeColor & saveNote

---
+++ lib/presentation/add_edit_note/add_edit_note_view_model.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:note_app_clone/domain/model/note.dart';
+import 'package:note_app_clone/domain/repository/note_repository.dart';
+import 'package:note_app_clone/presentation/add_edit_note/add_edit_note_event.dart';
+
+class AddEditNoteViewModel with ChangeNotifier {
+  final NoteRepository repository;
+
+  int _color = Colors.orange.value;
+
+  int get color => _color;
+
+  AddEditNoteViewModel(this.repository);
+
+  void onEvent(AddEditNoteEvent event) {
+    event.when(
+      changeColor: _changeColor,
+      saveNote: _saveNote,
+    );
+  }
+
+  Future<void> _changeColor(int color) async {
+    _color = color;
+    notifyListeners();
+  }
+
+  Future<void> _saveNote(int? id, String title, String content) async {
+    if (id == null) {
+      await repository.insertNote(
+        Note(
+          title: title,
+          content: content,
+          color: color,
+          timestamp: DateTime.now().millisecondsSinceEpoch,
+        ),
+      );
+    } else {
+      await repository.updateNote(
+        Note(
+          id: id,
+          title: title,
+          content: content,
+          color: color,
+          timestamp: DateTime.now().millisecondsSinceEpoch,
+        ),
+      );
+    }
+  }
+}
